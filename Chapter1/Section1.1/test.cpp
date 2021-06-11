debebd747fedbe998a2a8f29c6917f064e9c13a0
Yuzheng Ding <argentumdyz@gmail.com>
2021-06-11 12:27:21 +0800

    add test

---
+++ Chapter1/Section1.1/test.cpp
@@ -0,0 +1,22 @@
+/*
+ID: dyh2191
+LANG: C++14
+TASK: test
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	#ifndef ONLINE_JUDGE
+		freopen("test.in","r",stdin);
+		freopen("test.out","w",stdout);
+	#endif
+	int a,b;
+	cin>>a>>b;
+	int c = a + b;
+	cout<<c<<endl;
+	return 0;
+	
+}
