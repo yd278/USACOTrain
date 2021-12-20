1c0272cd107fae16dca14c0dfba0634e2a962547
calcite <argentumdyz@gmail.com>
2021-12-20 11:48:22 +0800

    correction of photo

--- PastPaper/2020DEC/bronze/photo.cpp
+++ PastPaper/2020DEC/bronze/photo.cpp
@@ -34,12 +34,16 @@ int main(){
             nextH = i;
         }
     }
-    int sum = 0;
+    long long sum = 0;
     for(int i = 0; i < n; i++){
-        sum += (i-Prev[i]) * (Next[i] - i);
+        sum += (long long)(i-Prev[i]) * (long long)(Next[i] - i);
     }
     sum -= n;
-    sum -= 2 * (n-1);
+    int cnt = 0;
+    for(int i = 1; i < n; i++){
+        if(s[i]!=s[i-1])cnt++;
+    }
+    sum -= cnt;
     cout<<sum<<endl;
     return 0;
 }
