8d06f332a3af922e0ba659bf28c51fd4e4bcf947
Y. Ding <argentumdyz@gmail.com>
2023-10-12 09:44:43 -0700

    fixTypo

--- PastPaper/2023/FEB/sliver/mooRoute.cpp
+++ PastPaper/2023/FEB/sliver/mooRoute.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 struct dept{
-	int t_dept;
+	int tDept;
 	int target;
-	int t_ariv;
+	int tAriv;
 	bool operator< (const dept &other) const{
-		if(t_dept<other.t_dept) return true;
-		if(t_dept==other.t_dept){
+		if(tDept<other.tDept) return true;
+		if(tDept==other.tDept){
 			if(target< other.target)return true;
 			if(target == other.target)
-				if(t_ariv < other.t_ariv) return true;
+				if(tAriv < other.tAriv) return true;
 		}
 		return false;
 	}
@@ -30,23 +30,24 @@ int main(){
 	vector<int> a(n+1);
 	for(int i = 1; i <= n; i++)cin>>a[i];
 	a[1] = 0;
-	vector<int> earist(n+1,INT_MAX);
+	vector<int> earliest(n+1,INT_MAX);
 	queue<ariv> q;
 	q.push({1,0});
 	while(!q.empty()){
 		auto qh = q.front();
 		q.pop();
-		if(earist[qh.city] > qh.time) earist[qh.city] = qh.time;
-		#define ds depts[qh.city]
-		auto itlow = ds.lower_bound({qh.time+a[qh.city],0,0});
-		for(auto it = itlow; it != ds.end();it++){
-			q.push({it->target,it->t_ariv});
+		if(earliest[qh.city] > qh.time) earliest[qh.city] = qh.time;
+
+		auto &ds = depts[qh.city];
+		auto itLow = ds.lower_bound({qh.time+a[qh.city],0,0});
+		for(auto it = itLow; it != ds.end();it++){
+			q.push({it->target,it->tAriv});
 		}
-		ds.erase(itlow,ds.end());
+		ds.erase(itLow,ds.end());
 	}
 	for(int i = 1; i <= n; i++){
-		if(earist[i] == INT_MAX) earist[i] = -1;
-		cout<<earist[i]<<endl;
+		if(earliest[i] == INT_MAX) earliest[i] = -1;
+		cout<<earliest[i]<<endl;
 	}
 	return 0;
 }
