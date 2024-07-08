f0808af67eeae127943555804b28cf1980a2f0e2
Yuzheng Ding <argentumdyz@gmail.com>
2024-07-07 17:32:26 -0700

    cereal

--- Luogu/1/4/1443.cpp
+++ Luogu/1/4/1443.cpp
@@ -2,8 +2,7 @@
 
 using namespace std;
 int steps[400][400];
-
-struct pos {
+struct Pos {
     int x, y;
 };
 int dx[8] = {1, 2, 2, 1, -1, -2, -2, -1};
@@ -23,27 +22,32 @@ int main() {
             steps[i][j] = -1; //-1 表示还没走到
         }
     }
-    queue<pos> q;
-    q.push(pos{x, y});
+    queue<Pos> q;
+    q.push(Pos{x, y});
     steps[x][y] = 0;
     while (!q.empty()) {
+
         auto head = q.front();
+
+        q.pop();
+
         for (int d = 0; d < 8; d++) {  // 枚举队首位置可以到达的所有位置
             int nx = head.x + dx[d];
             int ny = head.y + dy[d];
             if (nx >= 0 && nx < n && ny >= 0 && ny < m) {  // 如果位置合法
-                if (steps[nx][ny] ==
-                    -1) {  // if(!visited[nx][ny]) 且之前未搜到过
+
+                if (steps[nx][ny] ==-1) {  // if(!visited[nx][ny]) 且之前未搜到过
                     steps[nx][ny] = steps[head.x][head.y] + 1;  // 记录步数
-                    q.push(pos{nx, ny});  // 放进队列里
+                    q.push(Pos{nx, ny});  // 放进队列里
                 }
             }
         }
-        q.pop();
+
+        
     }
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) printf("%-5d", steps[i][j]);
-        printf("\n");
+        for (int j = 0; j < m; j++) cout<<steps[i][j]<<" ";
+        cout<<endl;
     }
     return 0;
 }
--- PastPaper/2022/JAN/silver/cereal.cpp
+++ PastPaper/2022/JAN/silver/cereal.cpp
@@ -1,92 +1,77 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-#define MAXN 100000
-#define MAXM 200000
 
-int match[MAXM+1];
-int first[MAXN+1];
-int second[MAXN+1];
-int selected[MAXN+1];
-int follow[MAXN+1];
-int inCnt[MAXN+1];
-int visited[MAXN+1];
-int dfs(int x,map<int,int> &vis){
-    int to = first[x];
-    if(!vis[to]){
-        vis[to] = 1;
-        if(!match[to]){
-            match[to] = x;
-            selected[x] = to;
-            return 1;
+struct Edge {
+    int cow;
+    int to;
+};
+vector<vector<Edge>> graph;
+vector<bool> visited_cycle;
+vector<bool> visited;
+vector<bool> not_hungry;
+vector<int> order;
+vector<int> first;
+int cycleCow;
+bool findCycle(int v, int pr) {
+    visited_cycle[v] = 1;
+    for (auto e : graph[v]) {
+        if (e.to == pr) continue;
+        if (visited_cycle[e.to]) {
+            cycleCow = e.cow;
+            return true;
         }
-        if(dfs(match[to],vis)){
-            match[to] = x;
-            selected[x] = to;
-            return 1;
+        if (findCycle(e.to, v)) return true;
+    }
+    return false;
+}
+void dfs(int v) {
+    visited[v] = 1;
+    for (auto e : graph[v]) {
+        if (visited[e.to]) continue;
+        if (e.cow == cycleCow) continue;
+        order.push_back(e.cow);
+        dfs(e.to);
+    }
+}
+int main() {
+// #ifndef ONLINE_JUDGE
+//     freopen("cereal.in", "r", stdin);
+//     freopen("cereal.out", "w", stdout);
+// #endif
+    int n, m;
+    cin >> n >> m;
+    graph.resize(m + 1);
+    visited_cycle.resize(m + 1);
+    visited.resize(m + 1);
+    not_hungry.resize(n + 1);
+    first.resize(n + 1);
+    int a, b;
+    for (int i = 1; i <= n; i++) {
+        cin >> a >> b;
+        graph[a].push_back({i, b});
+        graph[b].push_back({i, a});
+        first[i] = a;
+    }
+
+    for (int i = 1; i <= m; i++) {
+        if (!visited[i]) {
+            if (findCycle(i, 0)) {
+                order.push_back(cycleCow);
+                dfs(first[cycleCow]);
+            } else {
+                dfs(i);
+            }
         }
     }
-    to = second[x];
-    if(!vis[to]){
-        vis[to] = 1;
-        if(!match[to]){
-            match[to] = x;
-            selected[x] = to;
-            return 1;
-        }
-        if(dfs(match[to],vis)){
-            match[to] = x;
-            selected[x] = to;
-            return 1;
-        }
+
+    cout << n - order.size() << endl;
+    for (auto c : order) {
+        cout << c << endl;
+        not_hungry[c] = true;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (!not_hungry[i]) cout << i << endl;
     }
     return 0;
-
-}
-
-void check(int i){
-    if(visited[i])return;
-    visited[i] = 1;
-    if(follow[i]==0){
-        cout<<i<<endl;
-        return;
-    }
-    check(follow[i]);
-    cout<<i<<endl;
-}
-int main(){
-    // #ifndef ONLINE_JUDGE
-    //     freopen("cereal.in","r",stdin);
-    //     freopen("cereal.out","w",stdout);
-    // #endif
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    int n,m;
-    cin>>n>>m;
-    for(int i = 1; i <= n; i++){
-        cin>>first[i]>>second[i];
-    }
-    int cnt = 0;
-    for(int i = 1; i <= n; i++){
-        map<int,int>vis;
-        if(dfs(i,vis))cnt++;
-    }
-    cout<<n-cnt<<endl;
-    for(int i = 1; i < n; i++){
-        if(selected[i]==first[i]){
-            cout<<i<<endl;
-            visited[i] = 1;
-        }else{
-            follow[i] = match[first[i]];
-            inCnt[follow[i]]++;
-        }
-    }
-    for(int i = 1; i <= n; i++){
-        if(!visited[i]&&inCnt[i] == 0){
-            check(i);
-        }
-    }
-
-    return 0;
-
-
 }
