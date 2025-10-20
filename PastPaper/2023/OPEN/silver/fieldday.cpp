818328be52beae94cd333a8c8708951643cf4a12
Y. Ding <argentumdyz@gmail.com>
2025-10-19 18:58:26 -0700

    23OPEN optimize

--- PastPaper/2023/OPEN/silver/fieldday.cpp
+++ PastPaper/2023/OPEN/silver/fieldday.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
     int c, n;
     cin >> c >> n;
     vector<int> dist(1 << c, INT_MAX / 2);
     vector<int> team;
+    queue<int> q;
     for (int i = 0; i < n; i++) {
         int tmp = 0;
         for (int j = 0; j < c; j++) {
@@ -15,17 +15,21 @@ int main() {
             if (ch == 'G') tmp++;
         }
         team.push_back(tmp);
+        q.push(tmp);
         dist[tmp] = 0;
     }
-    for (int k = 0; k < c; k++) {
+    while (!q.empty()) {
+        int qh = q.front();
+        int d = dist[qh];
+        q.pop();
         for (int i = 0; i < c; i++) {
-            for (int x = 0; x < (1 << c); x++) {
-                if (dist[x] > dist[x ^ (1 << i)] + 1) {
-                    dist[x] = dist[x ^ (1 << i)] + 1;
-                }
+            if (dist[qh ^ (1 << i)] > d + 1) {
+                dist[qh ^ (1 << i)] = d + 1;
+                q.push(qh ^ (1 << i));
             }
         }
     }
+
     for (int t : team) {
         cout << c - dist[(1 << c) - 1 - t] << endl;
     }
